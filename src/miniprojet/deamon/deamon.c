6100490abed9b3994b8522266fd18480232714c0
Jean Nanchen <jean.nanchen@hevs.ch>
2022-06-17 16:42:43 +0200

    add oled and edit dtb

--- src/miniprojet/deamon/deamon.c
+++ src/miniprojet/deamon/deamon.c
@@ -51,6 +51,7 @@
 #include <sys/time.h>
 #include <sys/timerfd.h>
 #include <syslog.h>
+#include "ssd1306.h"
 
 #define GPIO_EXPORT   "/sys/class/gpio/export"
 #define GPIO_UNEXPORT "/sys/class/gpio/unexport"
@@ -209,6 +210,24 @@ int main(int argc, char* argv[])
     clock_gettime(CLOCK_MONOTONIC, &t1);
     char buff[10] = "";
     int k = 0;
+
+
+
+    ssd1306_init();
+
+    ssd1306_set_position (0,0);
+    ssd1306_puts("CSEL1a - SP.07");
+    ssd1306_set_position (0,1);
+    ssd1306_puts("  Demo - SW");
+    ssd1306_set_position (0,2);
+    ssd1306_puts("--------------");
+
+    ssd1306_set_position (0,3);
+    ssd1306_puts("Temp: 35'C");
+    ssd1306_set_position (0,4);
+    ssd1306_puts("Freq: 1Hz");
+    ssd1306_set_position (0,5);
+    ssd1306_puts("Duty: 50%");
 	//Infinity loop
 	while(1){
         FD_ZERO(&fd_in);
