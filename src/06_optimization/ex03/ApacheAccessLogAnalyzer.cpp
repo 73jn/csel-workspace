7aeff3241b2172ca50a2628f443ffb6c1e23c310
Jean Nanchen <jean.nanchen@hevs.ch>
2022-06-17 15:10:19 +0200

    Detect buttons, timer, deamon base

--- src/06_optimization/ex03/ApacheAccessLogAnalyzer.cpp
+++ src/06_optimization/ex03/ApacheAccessLogAnalyzer.cpp
@@ -13,7 +13,12 @@ ApacheAccessLogAnalyzer::ApacheAccessLogAnalyzer(std::string filename)
 
 void ApacheAccessLogAnalyzer::openFile()
 {
+    //std::cout << "Open" << std::endl;
     myInFile = fopen(myFilename.c_str(), "r");
+    if( myInFile == NULL ) {
+        std::cout << "Error" << std::endl;
+        exit(1);
+    }
 }
 
 void ApacheAccessLogAnalyzer::closeFile()
@@ -24,12 +29,13 @@ void ApacheAccessLogAnalyzer::closeFile()
 void ApacheAccessLogAnalyzer::processFile()
 {
     std::ios::sync_with_stdio(false);
-    char* line;
+    char line[300];
     std::string linecopy;
     std::cout << "Processing log file " << myFilename << std::endl;
-    while((fgets(line, 100, myInFile)))
+    while((fgets(line, 300, myInFile)))
     {
         linecopy = line;
+        //std::cout << (linecopy.substr(0, linecopy.find_first_of(" "))) << std::endl;
         //myHostCounter.notifyHost(line.substr(0, line.find_first_of(" ")));
         myHostCounter.myHosts.insert((linecopy.substr(0, linecopy.find_first_of(" ")))); //INLINING
     }
--- src/miniprojet/deamon/deamon.c
+++ src/miniprojet/deamon/deamon.c
@@ -39,9 +39,69 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/timerfd.h>
+#include <syslog.h>
+
+#define GPIO_EXPORT   "/sys/class/gpio/export"
+#define GPIO_UNEXPORT "/sys/class/gpio/unexport"
+#define K1            "0"
+#define K2            "2"    
+#define K3            "3"
+#define GPIO_K1     "/sys/class/gpio/gpio0"
+#define GPIO_K2     "/sys/class/gpio/gpio2"
+#define GPIO_K3     "/sys/class/gpio/gpio3"
+
 #define UNUSED(x) (void)(x)
 
 static int signal_catched = 0; 
+
+static int open_gpio(char* gpio_n, char* direction, char* path){
+    char dest[50] = "";
+    // unexport pin out of sysfs (reinitialization)
+    int f = open(GPIO_UNEXPORT, O_WRONLY);
+    write(f, gpio_n, strlen(gpio_n));
+    close(f);
+
+    // export pin to sysfs
+    f = open(GPIO_EXPORT, O_WRONLY);
+    write(f, gpio_n, strlen(gpio_n));
+    close(f);
+
+    // config pin
+    strcpy(dest, path);
+    strcat(dest, "/direction");
+    f = open(dest, O_WRONLY);
+    write(f, direction, 3);
+    close(f);
+    if (strcmp(direction, "in")==0)
+    {
+        printf("Went into the rising for %s\n", gpio_n);
+        // config pin
+        strcpy(dest, path);
+        strcat(dest, "/edge");
+        f = open(dest, O_WRONLY);
+        write(f, "rising", strlen("rising"));
+        close(f);
+    }
+
+
+    // open gpio value attribute
+    strcpy(dest, path);
+    strcat(dest, "/value");
+    f = open(dest, O_RDWR);
+	syslog(LOG_INFO, "Open GPIO\n");
+    return f;
+}
 
 static void catch_signal (int signal)
 {
@@ -85,7 +145,7 @@ int main(int argc, char* argv[])
 	sigaction (SIGTSTP, &act, NULL);  // 19 - terminal stop signal
 
 	// 5. update file mode creation mask
-	umask(0027);
+	umask(777);
 
 	// 6. change working directory to appropriate place
 	if (chdir ("/") == -1) {
@@ -117,6 +177,7 @@ int main(int argc, char* argv[])
 	syslog (LOG_INFO, "Daemon has started...");
  
 	// 10. option: get effective user and group id for appropriate's one
+	/*
 	struct passwd* pwd = getpwnam ("daemon");
 	if (pwd == 0) {
 		syslog (LOG_ERR, "ERROR while reading daemon password file entry");
@@ -138,14 +199,84 @@ int main(int argc, char* argv[])
 		syslog (LOG_ERR, "ERROR while setting new effective user id");
 		exit (1);
 	}
+	*/
 
 	// 13. implement daemon body...
-	int t = 30;
-	do {
-		t = sleep(t);
-	} while (t > 0);
 
-	syslog (LOG_INFO, "daemon stopped. Number of signals catched=%d\n", signal_catched);
+	//Init
+    fd_set fd_in, fd_out, fd_except;
+
+    setlogmask(LOG_UPTO(LOG_INFO));
+
+    openlog("log_interval", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+
+    int k1 = open_gpio(K1, "in", GPIO_K1);
+    int k2 = open_gpio(K2, "in", GPIO_K2);
+    int k3 = open_gpio(K3, "in", GPIO_K3);
+	syslog(LOG_INFO, "Done GPIO\n");
+    int tfd = timerfd_create(CLOCK_MONOTONIC, 0);
+    long period = 1000;  // ms
+    period *= 1000000;  // in ns
+    long time_interval = period / 2;
+
+    struct itimerspec spec = {
+        .it_interval = {0, 0},
+        .it_value = {0, time_interval}
+    };
+    timerfd_settime(tfd, 0, &spec, NULL);
+
+
+    int largest_fd = (k1 > k2) ? k1 : k2;
+    largest_fd = (largest_fd > k3) ? largest_fd : k3;
+    largest_fd = (largest_fd > tfd) ? largest_fd : tfd;
+
+    struct timespec t1;
+    clock_gettime(CLOCK_MONOTONIC, &t1);
+    char buff[10] = "";
+    int k = 0;
+	//Infinity loop
+	while(1){
+        FD_ZERO(&fd_in);
+        FD_ZERO(&fd_out);
+        FD_ZERO(&fd_except);
+        FD_SET(k1, &fd_except);
+        FD_SET(k2, &fd_except);
+        FD_SET(k3, &fd_except);
+        FD_SET(tfd, &fd_in);
+        int ret = select (largest_fd+1, &fd_in, &fd_out, &fd_except, NULL);
+        // check if select actually succeed
+        if (ret == (-1)) {
+        // report error and abort
+        } 
+        else if (ret == 0) {
+        // timeout; no event detected
+        } else {
+            if (FD_ISSET(k1, &fd_except)) {
+                pread(k1, buff, sizeof("1"), 0);
+                printf("K1 = 1\n");
+                syslog(LOG_INFO, "Time interval increased : %d", time_interval);
+                }
+            if (FD_ISSET(k2, &fd_except)) {
+                pread(k2, buff, sizeof("1"), 0);
+                printf("K2 pressed\n");
+                syslog(LOG_INFO, "Time interval reset : %d", time_interval);
+                }
+            if (FD_ISSET(k3, &fd_except)) {
+                pread(k3, buff, sizeof("1"), 0);
+                printf("K3 pressed\n");
+                syslog(LOG_INFO, "Time interval decreased : %d", time_interval);
+            }
+            if (FD_ISSET(tfd, &fd_in)) {
+                pread(tfd, buff, 8, 0);
+                printf("timer occurred %d\n", time_interval);
+                //spec.it_value.tv_nsec = time_interval;
+                timerfd_settime(tfd, 0, &spec, NULL);
+				syslog(LOG_INFO, "Timer occured.\n");
+            }
+        } 
+	}
+
+	syslog (LOG_INFO, "daemon stopped V2. Number of signals catched=%d\n", signal_catched);
 	closelog();
 
 	return 0;
